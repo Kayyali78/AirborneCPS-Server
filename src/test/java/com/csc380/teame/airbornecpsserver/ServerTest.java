8b65c79686e650ebd291214162856ec13732cb77
Kayyali78 <sweeks@oswego.edu>
2022-05-06 20:26:21 -0400

    Deleted Unused Old class Files

--- src/test/java/com/csc380/teame/airbornecpsserver/ServerTest.java
+++ src/test/java/com/csc380/teame/airbornecpsserver/ServerTest.java
@@ -1,22 +1,21 @@
-package com.csc380.teame.airbornecpsserver;
-
-import junit.framework.TestCase;
-
-import java.io.IOException;
-
-public class ServerTest{
-    String test1 = "java -jar tcpbeacons.jar -server";
-    String test2 = "java -jar tcpbeacons.jar -server 1901 northbound.txt slow";
-    String test3 = "java -jar tcpbeacons.jar -server 1901 southbound.txt slow slow";
-    String test4 = "java -jar tcpbeacons.jar -server 1901 northbound.txt";
-    String test5 = "java -jar tcpbeacons.jar -server 1901";
-
-    String test6 = "java -jar tcpbeacons.jar -client 127.0.0.1 1901 northbound.txt";
-    String test7 = "java -jar tcpbeacons.jar -client 127.0.0.1 1901 southbound.txt slow";
-    String test8 = "java -jar tcpbeacons.jar -client 127.0.0.1 1901 northbound.txt slow slow";
-
-    public void testServe() throws InterruptedException {
-        Server server = new Server();
-        server.serve();
-    }
-}
+//package com.csc380.teame.airbornecpsserver;
+//
+//import junit.framework.TestCase;
+//
+//import java.io.IOException;
+//
+//public class ServerTest{
+//    String test1 = "java -jar tcpbeacons.jar -server";
+//    String test2 = "java -jar tcpbeacons.jar -server 1901 northbound.txt slow";
+//    String test3 = "java -jar tcpbeacons.jar -server 1901 southbound.txt slow slow";
+//    String test4 = "java -jar tcpbeacons.jar -server 1901 northbound.txt";
+//    String test5 = "java -jar tcpbeacons.jar -server 1901";
+//
+//    String test6 = "java -jar tcpbeacons.jar -client 127.0.0.1 1901 northbound.txt";
+//    String test7 = "java -jar tcpbeacons.jar -client 127.0.0.1 1901 southbound.txt slow";
+//    String test8 = "java -jar tcpbeacons.jar -client 127.0.0.1 1901 northbound.txt slow slow";
+//
+//    public void testServe() throws InterruptedException {
+//        TCPHandler = new TCPHandler()
+//    }
+//}
